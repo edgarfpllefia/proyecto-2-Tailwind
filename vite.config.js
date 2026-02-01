5e62aca04f543bc459056da0c7c66df7ba4fb7c8
edgarfpllefia <morenocomasedgar@fpllefia.com>
2026-02-01 01:35:25 +0100

    b

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,19 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from 'path';
+
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        article: resolve(__dirname, 'article.html'),
+        careers: resolve(__dirname, 'careers.html'),
+        journal: resolve(__dirname, 'journal.html'),
+        'job-listing': resolve(__dirname, 'job-listing.html'),
+      }
+    }
+  }
 });
